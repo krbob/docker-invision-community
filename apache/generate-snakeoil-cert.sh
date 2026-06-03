4a00ffda5f6601d7b388573e5f3a597f3e670a8f
Krzysztof Bobiński <krzysztof@bobinski.net>
2026-06-03 12:10:00 +0200

    Harden snakeoil cert script

--- apache/generate-snakeoil-cert.sh
+++ apache/generate-snakeoil-cert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -euo pipefail
 
 CERT_DIR=${HTTPD_PREFIX}/conf/
-openssl genpkey -algorithm RSA -out ${CERT_DIR}/snakeoil.key -pkeyopt rsa_keygen_bits:2048
-openssl req -new -x509 -key ${CERT_DIR}/snakeoil.key -out ${CERT_DIR}/snakeoil.crt -days 365 -subj "/C=PL/ST=Test/L=Test/O=Test/OU=Test/CN=localhost"
+openssl genpkey -algorithm RSA -out "${CERT_DIR}/snakeoil.key" -pkeyopt rsa_keygen_bits:2048
+openssl req -new -x509 -key "${CERT_DIR}/snakeoil.key" -out "${CERT_DIR}/snakeoil.crt" -days 365 -subj "/C=PL/ST=Test/L=Test/O=Test/OU=Test/CN=localhost"
